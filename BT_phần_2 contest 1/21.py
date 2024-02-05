f8004656bfc45de6198cbdcb597d98d6f82c773e
tranquoctrung257 <tt3617089@gmail.com>
2024-02-06 05:46:05 +0700

    update

---
+++ BT_phần_2 contest 1/21.py
@@ -0,0 +1,9 @@
+a,b,k = map(int,input().split())
+left,right = 0,0
+if k % 2 == 0:
+    left = k // 2
+    right =  k // 2
+else:
+    left = k // 2 
+    right = left + 1 
+print(right * a - left * b) 
