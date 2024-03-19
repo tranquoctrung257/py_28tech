3baff7bb373b92d1d9386d2e0a1e8845935612cf
tranquoctrung257 <tt3617089@gmail.com>
2024-03-20 05:28:42 +0700

    update

--- BT_phần_4 contest 3/1.py
+++ BT_phần_4 contest 3/1.py
@@ -1,14 +1,12 @@
 import math
-def ngto(n):
-    if n > 2:
-        return False
+
+def prime(n):
+    if n < 2: return False
     for i in range(2,math.isqrt(n)+1):
         if n % i == 0:
             return False
     return True
 if __name__ == "__main__":
     n = int(input())
-    if ngto(n):
-        print("YES")
-    else:
-        print("NO")
+    if prime(n):print("YES")
+    else:print("NO")
--- BT_phần_4 contest 3/2.py
+++ BT_phần_4 contest 3/2.py
@@ -1,1 +1,1 @@
-# 
+from math
